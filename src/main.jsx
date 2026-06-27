9328a5ed7c06bccb88da4cc8a1bbed893b94f06d
samarth-thaker <samarth.thaker07@gmail.com>
2026-06-27 12:35:10 +0530

    project init

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import "./styles/variable.css";
+import "./styles/global.css";
+import "./styles/typography.css";
+import "./styles/animation.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
